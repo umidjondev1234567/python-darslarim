3c8997013f1b62374cf07a22978a7a3b093c5894
umidjondev1234567 <103695341+umidjondev1234567@users.noreply.github.com>
2022-04-15 11:16:01 +0500

    yangi fayl
    list.py

---
+++ spyder/list2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 04:40:14 2022
+
+@author: User
+"""
+
+# imkon = ['non', 'ukki', 2] 
+# print(imkon)
+
+# cars = ['BMW', 'audi', 'mersades Benz', "GM", "marko polo"]
+# cars.sort()
+# print(cars)
+
+# cars = ['BMW', 'audi', 'mersades Benz', "GM", "marko polo"]
+# cars.sort(reverse=True)
+# print(cars)
+
+# cars = ['BMW', 'audi', 'mersades Benz', "GM", "marko polo"]
+# print(sorted(cars)) # print(sorted(cars, reverse=True)) __teskarisiga
+
+# 
+# sonlar = [1, 2, 6, -5.5, 88]
+# print(sorted(sonlar, reverse=True))
+
+
+# davlat = ['afgon', "uzbek", "Russia", "malta"]
+# davlat.reverse()
+# print(davlat)
+# print(len(davlat))
+
+
+# print(list(range(1,30)))
+
+# toq_sonlar = list(range(1, 20, 2))
+# print(toq_sonlar)
+
+# max_qiymat = max('1', '50')
+# print(max_qiymat)
+
+
+# royhatni kesish
+# sonlar = list(range(2, 20, 3))
+# print(sonlar)
+# kesilgan = sonlar[0:4]
+# print(kesilgan)
+
